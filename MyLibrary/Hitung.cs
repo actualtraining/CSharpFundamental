296d028de3bb2a838ce7980667c767bf71d51b09
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-25 14:17:23 +0700

    out param

--- MyLibrary/Hitung.cs
+++ MyLibrary/Hitung.cs
@@ -16,7 +16,22 @@ namespace MyLibrary
     {
         public double HitungSegitiga(double alas,double tinggi)
         {
+            alas = 14;
+            tinggi = 14;
             return 0.5 * alas * tinggi;
+        }
+
+        public double HitungLingkaran(ref double jari)
+        {
+            jari = 20;
+            return 3.14 * jari * jari;
+        }
+
+        public double HitungPersegi(double panjang,double lebar,out string outparam)
+        {
+            outparam = "ditambahkan di method HitungPersegi";
+            double hasil = panjang * lebar;
+            return hasil;    
         }
     }
 }
--- SampleType/Program.cs
+++ SampleType/Program.cs
@@ -26,7 +26,7 @@ namespace SampleType
             double hasil = objHitung.HitungSegitiga(alas, tinggi);
             Console.WriteLine("Hasilnya adalah : "+hasil);*/
 
-            int a = 5;
+            /*int a = 5;
             int b = a;
             a += 5;
             Console.WriteLine("a: " + a);
@@ -55,8 +55,61 @@ namespace SampleType
                 case Hari.selasa:
                     Console.WriteLine("Hari selasa");
                     break;
+            }*/
+
+            //narrowing and widening
+            //int a = 5;
+            //double b = a;
+
+            //double nilai1 = 2.45;
+            ////convert
+            //int nilai2 = Convert.ToInt32(nilai1);
+            ////casting
+            //int nilai3 = (int)nilai1;
+
+            //Console.WriteLine(nilai2);
+
+            //var nama = (nilai1 > 5) ? "hello" : "world";
+
+            /*string[] arrNama = new string[5];
+            arrNama[0] = "Erick";
+            arrNama[1] = "Budi";
+            arrNama[2] = "Deny";
+            arrNama[3] = "Dewi";
+            arrNama[4] = "Bambang";
+
+            int[] arrNumber = new int[] { 3, 4, 55, 67, 34 };
+            
+            for(int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine(arrNama[i]);
             }
-         
+
+            foreach(int number in arrNumber)
+            {
+                Console.WriteLine(number);
+            }*/
+
+            double alas, tinggi, jari;
+            alas = 12;
+            tinggi = 12;
+            Hitung objHitung = new Hitung();
+            double hasil = objHitung.HitungSegitiga(alas, tinggi);
+            jari = 25;
+            double luas = objHitung.HitungLingkaran(ref jari);
+
+            Console.WriteLine(alas);
+            Console.WriteLine(tinggi);
+            Console.WriteLine(jari);
+
+            Console.WriteLine("hasil :" + hasil);
+            Console.WriteLine("hasil luas lingkaran :" + jari);
+
+            string outparam = "";
+            double hasilpersegi = objHitung.HitungPersegi(12, 14, out outparam);
+
+            Console.WriteLine("Luas Persegi :" + hasilpersegi);
+            Console.WriteLine(outparam);
         }
     }
 }
